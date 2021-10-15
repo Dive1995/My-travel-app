3354d4e23874e06608e63c791450d7da59b9eaf7
Diveshan <diveshan1995@gmail.com>
2021-10-15 21:54:09 +0530

    added link to website

--- src/components/PlaceDetails.jsx
+++ src/components/PlaceDetails.jsx
@@ -9,23 +9,24 @@ function PlaceDetails({place,selected, refProp, i}) {
     }
 
     return (
-        <div className="card" >
-        <div className="card-img">
-            <img src={ place.photo ? place.photo.images.large.url : "https://youmatter.world/app/uploads/sites/2/2019/11/travel-world.jpg"} alt="" />
-        </div>
-        <div className="card-detail">
-            <h4>{place.name}</h4>
-            <p>{place.address}</p>
-            <p>{place.price}</p>
-            <p>{place.phone}</p>
-            <Rating
-            emptySymbol='fa fa-star-o yellow '
-            fullSymbol='fa fa-star yellow '
-            initialRating={Number(place.rating)}
-            readonly
-            />
-        </div>
-    </div>
+        <a href={place?.website || place?.web_url} className="card" >
+            <div className="card-img">
+                <img src={ place.photo ? place.photo.images.large.url : "https://youmatter.world/app/uploads/sites/2/2019/11/travel-world.jpg"} alt="" />
+            </div>
+            <div className="card-detail">
+                <h4>{place.name}</h4>
+                <p>{place.address}</p>
+                <p>{place.price}</p>
+                <p>{place.phone}</p>
+                <Rating
+                emptySymbol='fa fa-star-o yellow '
+                fullSymbol='fa fa-star yellow '
+                initialRating={Number(place.rating)}
+                readonly
+                />
+                out of {place.num_reviews} reviews
+            </div>
+        </a>
     )
 }
 
